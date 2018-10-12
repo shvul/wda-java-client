cffc5f241314b46f41db08d216d782349a7b24e6
Pavel Serdiukov <iamshvul@gmail.com>
2018-10-12 17:37:32 +0300

    add json converter

---
+++ src/main/java/com/github/shvul/wda/client/support/JsonConverter.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.shvul.wda.client.support;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonConverter {
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    static {
+        mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+    }
+
+    private JsonConverter() {
+    }
+
+    public static <T> T fromJson(String json, Class<T> classOfT) {
+        try {
+            return mapper.readValue(json, classOfT);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public static String toJson(Object src) {
+        try {
+            return mapper.writeValueAsString(src);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+}
