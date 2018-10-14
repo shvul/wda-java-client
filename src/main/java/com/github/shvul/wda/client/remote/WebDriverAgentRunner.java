608fa2a7a13883c795fb1f1927d7a0bb5bcc38b3
Pavel Serdiukov <iamshvul@gmail.com>
2018-10-14 19:39:27 +0300

    add wda runner

---
+++ src/main/java/com/github/shvul/wda/client/remote/WebDriverAgentRunner.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.shvul.wda.client.remote;
+
+import com.github.shvul.wda.client.support.LoggerManager;
+import com.github.shvul.wda.client.driver.DriverCapabilities;
+import com.github.shvul.wda.client.exception.WebDriverAgentException;
+import com.google.common.util.concurrent.SimpleTimeLimiter;
+import org.apache.http.HttpStatus;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class WebDriverAgentRunner {
+    private static final String WDA_BUNDLE_ID = "com.apple.test.WebDriverAgentRunner_tvOS-Runner";
+    private static final String WDA_BASE_URL = "http://localhost";
+    private static final int WDA_AGENT_PORT = 8100;
+    private static final int WDA_LAUNCH_TIMEOUT = 60;
+    private static final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private DriverCapabilities capabilities;
+    private Process wdaProcess;
+
+    public WebDriverAgentRunner(DriverCapabilities capabilities) {
+        this.capabilities = capabilities;
+    }
+
+    public void start() {
+        LoggerManager.info("Start WebDriverAgent.");
+        wdaProcess = new XCodeBuilder()
+                .setWdaPath(capabilities.getCapability(DriverCapabilities.Key.WDA_PATH))
+                .setPlatform(capabilities.getCapability(DriverCapabilities.Key.PLATFORM))
+                .setDeviceName(capabilities.getCapability(DriverCapabilities.Key.DEVICE_NAME))
+                .setDeviceId(capabilities.getCapability(DriverCapabilities.Key.DEVICE_NAME))
+                .setOsVersion(capabilities.getCapability(DriverCapabilities.Key.OS_VERSION))
+                .build();
+
+        waitForReachability(getWdaUrl(), WDA_LAUNCH_TIMEOUT);
+    }
+
+    public void stop() {
+        LoggerManager.info("Stop WebDriverAgent.");
+        Optional.ofNullable(wdaProcess).ifPresent(Process::destroyForcibly);
+    }
+
+    public URL getWdaUrl() {
+
+        String urlStr = Optional.ofNullable(capabilities.getCapability(DriverCapabilities.Key.WDA_URL))
+                .map(url -> String.format("%s:%s", url, WDA_AGENT_PORT))
+                .orElse(String.format("%s:%s", WDA_BASE_URL, WDA_AGENT_PORT));
+        try {
+            return new URL(urlStr);
+        } catch (MalformedURLException e) {
+            throw new WebDriverAgentException("Url syntax is malformed: " + urlStr);
+        }
+    }
+
+    private void waitForReachability(URL url, int timeout) {
+        try {
+            SimpleTimeLimiter.create(executorService).callWithTimeout(() -> {
+                HttpURLConnection connection = null;
+                long sleepMillis = 10L;
+                while (true) {
+                    try {
+                        connection = this.connectToUrl(url);
+                        if (connection.getResponseCode() == HttpStatus.SC_OK) {
+                            return connection.getResponseCode();
+                        }
+                    } finally {
+                        if (connection != null) {
+                            connection.disconnect();
+                        }
+                    }
+
+                    TimeUnit.MILLISECONDS.sleep(sleepMillis);
+                    sleepMillis = sleepMillis >= 320L ? sleepMillis : sleepMillis * 2L;
+                }
+            }, timeout, TimeUnit.SECONDS);
+        } catch (TimeoutException | InterruptedException | ExecutionException e) {
+            throw new WebDriverAgentException(String.format("WDA is not reachable in %d seconds.", timeout), e);
+        }
+    }
+
+    private HttpURLConnection connectToUrl(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setConnectTimeout(500);
+        connection.setReadTimeout(1000);
+        connection.connect();
+        return connection;
+    }
+}
