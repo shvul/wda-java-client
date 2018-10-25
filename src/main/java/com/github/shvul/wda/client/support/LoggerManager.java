7638d411995feefe449dc11fe7f8f579f449d49a
Pavel Serdiukov <iamshvul@gmail.com>
2018-10-25 13:42:14 +0300

    update logger manager

--- src/main/java/com/github/shvul/wda/client/support/LoggerManager.java
+++ src/main/java/com/github/shvul/wda/client/support/LoggerManager.java
@@ -26,12 +26,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class LoggerManager {
-    private static final String WDA_LOGGER_NAME = "WDA Client";
-    private static final String CONSOLE_APPENDER_NAME = "Console";
-    private static final Logger LOGGER = LoggerFactory.getLogger(WDA_LOGGER_NAME);
+    public static final String CLIENT_LOGGER_NAME = "WDA Client";
+    public static final String SERVER_LOGGER_NAME = "WDA Server";
+    private static final String CLIENT_CONSOLE_APPENDER_NAME = "Console";
+    private static final String SERVER_CONSOLE_APPENDER_NAME = "Console-Xcode";
+    private static final Logger LOGGER = LoggerFactory.getLogger(CLIENT_LOGGER_NAME);
 
     public static Logger getLogger() {
         return LOGGER;
+    }
+
+    public static Logger getLogger(String name) {
+        return LoggerFactory.getLogger(name);
     }
 
     public static void info(String message) {
@@ -58,11 +64,19 @@ public final class LoggerManager {
         LOGGER.error(message, throwable);
     }
 
-    public static void enableConsoleLogging() {
+    public static void enableClientConsoleLogging() {
+        enableConsoleLogging(CLIENT_LOGGER_NAME, CLIENT_CONSOLE_APPENDER_NAME);
+    }
+
+    public static void enableServerConsoleLogging() {
+        enableConsoleLogging(SERVER_LOGGER_NAME, SERVER_CONSOLE_APPENDER_NAME);
+    }
+
+    public static void enableConsoleLogging(String loggerName, String appenderName) {
         LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
         Configuration configuration = loggerContext.getConfiguration();
-        LoggerConfig wdaLogger = configuration.getLoggerConfig(WDA_LOGGER_NAME);
-        Appender appender = configuration.getAppender(CONSOLE_APPENDER_NAME);
+        LoggerConfig wdaLogger = configuration.getLoggerConfig(loggerName);
+        Appender appender = configuration.getAppender(appenderName);
         wdaLogger.addAppender(appender, Level.ALL, null);
     }
 }
